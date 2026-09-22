65cbb2c7773eab395b3455668738fdaee03ea260
gitchubst <gitchub@gmail.com>
2026-09-22 01:40:10 -0400

    Turn off surprise mode by default

--- Website/lib/demo-data.ts
+++ Website/lib/demo-data.ts
@@ -5,7 +5,7 @@ export const DEFAULT_SETTINGS: FeedSettings = {
   obscurity: 5,
   displayMode: "picture-text",
   sentenceLength: 3,
-  surpriseMe: true
+  surpriseMe: false
 };
 
 export function createDefaultTopics(): TopicNode[] {
